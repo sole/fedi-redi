6f62aa58bf74e3892fa252226e7eaeba516923ea
sole <listas@soledadpenades.com>
2023-08-29 19:01:07 +0100

    redirect user rather than proxying

--- user.php
+++ user.php
@@ -2,13 +2,14 @@
 require_once('config.php');
 require_once('miniproxy.php');
 
-// Proxy from XYZ.domain/@user to domain/@user
+// Redirect from XYZ.domain/@user to domain/@user
 $user = $_GET['user'];
 
 if(strlen($user) === 0) {
-	echo 'Did not find a user to proxy to...';
+	echo 'Did not find a user to redirect to...';
 	die;
 }
 
-$dst_url = $fediredi_config['redirect_to_server'] . '/' . '@' . $user;
-miniproxy($dst_url);
+$dst_url = $fediredi_config['redirect_to_server'] . '@' . $user;
+header('Location: '. $dst_url);
+die;
