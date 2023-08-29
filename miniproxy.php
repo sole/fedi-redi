1f994fe3694f7b660af37529dcdb8e5fa6a7f67f
sole <listas@soledadpenades.com>
2023-08-29 18:08:00 +0100

    Catch more errors

--- miniproxy.php
+++ miniproxy.php
@@ -3,20 +3,27 @@
 function miniproxy($url) {
 	$response_headers = array();
 
-	$fp = fopen($url, 'r');
+	try {
+		@ $fp = fopen($url, 'r');
+		if($fp === false) {
+			throw new Exception('that URL cannot be opened');
+		}
 
-	$meta = stream_get_meta_data($fp);
-	if($meta['wrapper_data']) {
-		$response_headers = $meta['wrapper_data'];
+		$meta = stream_get_meta_data($fp);
+		if($meta['wrapper_data']) {
+			$response_headers = $meta['wrapper_data'];
+		}
+
+		$content = stream_get_contents($fp);
+
+		foreach($response_headers as $h) {
+			header($h);
+		}
+		echo $content;
+
+		fclose($fp);
+
+	} catch (Exception $e) {
+		echo 'Error: ' . $e->getMessage();
 	}
-
-	$content = stream_get_contents($fp);
-
-	foreach($response_headers as $h) {
-		header($h);
-	}
-	echo $content;
-
-	fclose($fp);
-
 }
